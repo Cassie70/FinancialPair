26c439f9dfee9a01e77b36a1dcc51c7da2d165fd
cassie70 <yael07mora@gmail.com>
2026-07-05 17:04:08 -0600

    evitar topics duplicados

--- app/src/main/java/com/example/financialpair/data/entity/Topic.kt
+++ app/src/main/java/com/example/financialpair/data/entity/Topic.kt
@@ -2,9 +2,11 @@ package com.example.financialpair.data.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
+    indices = [Index("name", unique = true)],
     foreignKeys = [
         ForeignKey(
             entity = Category::class,
